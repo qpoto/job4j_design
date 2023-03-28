33bf9e8851c71354305e50e89adf80fff84663b2
qpoto <qpoto@yandex.ru>
2023-03-28 08:47:27 +0300

    1. Динамический список на массиве. [#158]

--- src/main/java/ru/job4j/collection/SimpleArrayList.java
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -1,16 +1,11 @@
 package ru.job4j.collection;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-import java.util.Objects;
+import java.util.*;
 
 public class SimpleArrayList<T> implements SimpleList<T> {
-
     private T[] container;
     private int size;
     private int modCount;
-
 
     public SimpleArrayList(int capacity) {
         container = (T[]) new Object[capacity];
@@ -18,55 +13,75 @@ public class SimpleArrayList<T> implements SimpleList<T> {
 
     @Override
     public void add(T value) {
-        container[size++] = value;
         modCount++;
+        if (size == container.length) {
+            container = grow();
+        }
+        container[size++] = value;
+    }
+
+    private T[] grow() {
+        return Arrays.copyOf(container, container.length * 2 + 10);
     }
 
     @Override
     public T set(int index, T newValue) {
-        if (index < container.length) {
-            container[index] = newValue;
-            size++;
-            modCount++;
-        }
-        return container[index];
+        Objects.checkIndex(index, size);
+        T oldValue = container[index];
+        container[index] = newValue;
+        return oldValue;
     }
-
 
     @Override
     public T remove(int index) {
-        if (index > -1) {
-            container[index] = null;
-            modCount++;
-        }
-        return container[index];
+        Objects.checkIndex(index, size);
+        modCount++;
+        T res = container[index];
+        System.arraycopy(
+                container, index + 1,
+                container,
+                index,
+                container.length - index - 1
+        );
+        container[--size] = null;
+        return res;
     }
 
     @Override
     public T get(int index) {
+        Objects.checkIndex(index, size);
         return container[index];
     }
 
     @Override
     public int size() {
-        return container.length;
+        return size;
     }
 
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
+            int cursor = 0;
+            final int expectedModCount = modCount;
 
             @Override
             public boolean hasNext() {
-                return true;
+                checkForComodification();
+                return cursor < size;
             }
 
             @Override
             public T next() {
+                checkForComodification();
                 if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
-                return container[size++];
+                return container[cursor++];
+            }
+
+            final void checkForComodification() {
+                if (modCount != expectedModCount)
+                    throw new ConcurrentModificationException();
             }
         };
     }
