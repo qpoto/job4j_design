a8ddb760fc824240d2daefe216b10a70bd38c7a8
qpoto <qpoto@yandex.ru>
2023-04-26 22:42:48 +0300

    1. Создать элементарную структуру дерева [#1711]

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,6 +1,7 @@
 package ru.job4j.tree;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
@@ -9,28 +10,26 @@ public class SimpleTree<E> implements Tree<E> {
         this.root = new Node<>(root);
     }
 
+
     @Override
     public boolean add(E parent, E child) {
         boolean rsl = false;
-        Optional<Node<E>> findParent = findBy(parent);
-        if (findParent.isPresent()) {
-            Node<E> val = findParent.get();
-            if (val.value == parent && !val.children.contains(child)) {
-                val.children.add(new Node<>(child));
-                rsl = true;
-            }
+        Optional<Node<E>> parentNode = findBy(parent);
+        Optional<Node<E>> childNode = findBy(child);
+        if (parentNode.isPresent() && childNode.isEmpty()) {
+            parentNode.get().children.add(new Node<>(child));
+            rsl = true;
         }
         return rsl;
     }
 
-    @Override
-    public Optional<Node<E>> findBy(E value) {
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
         Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.value.equals(value)) {
+            if (condition.test(el)) {
                 rsl = Optional.of(el);
                 break;
             }
@@ -38,4 +37,15 @@ public class SimpleTree<E> implements Tree<E> {
         }
         return rsl;
     }
+
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        return findByPredicate(it -> it.value.equals(value));
+    }
+
+    public boolean isBinary() {
+        return findByPredicate(it -> it.children.size() > 2).isEmpty();
+    }
+
 }
--- src/test/java/ru/job4j/tree/SimpleTreeTest.java
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -1,10 +1,11 @@
 package ru.job4j.tree;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 public class SimpleTreeTest {
-
     @Test
     void when6ElFindLastThen6() {
         Tree<Integer> tree = new SimpleTree<>(1);
@@ -15,14 +16,12 @@ public class SimpleTreeTest {
         tree.add(5, 6);
         assertThat(tree.findBy(6)).isPresent();
     }
-
     @Test
     void whenElFindNotExistThenOptionEmpty() {
         Tree<Integer> tree = new SimpleTree<>(1);
         tree.add(1, 2);
         assertThat(tree.findBy(7)).isEmpty();
     }
-
     @Test
     void whenChildExistOnLeafThenNotAdd() {
         Tree<Integer> tree = new SimpleTree<>(1);
@@ -32,5 +31,28 @@ public class SimpleTreeTest {
         tree.add(4, 5);
         tree.add(5, 6);
         assertThat(tree.add(2, 6)).isFalse();
+    }
+    @Test
+    public void testIsBinaryWhenTreeIsBinary() {
+        SimpleTree<Integer> tree = new SimpleTree<>(1);
+        assertTrue(tree.add(1, 2));
+        assertTrue(tree.add(1, 3));
+        assertTrue(tree.add(2, 4));
+        assertTrue(tree.add(2, 5));
+        assertTrue(tree.add(3, 6));
+        assertTrue(tree.add(3, 7));
+        assertTrue(tree.isBinary());
+    }
+    @Test
+    public void testIsBinaryWhenTreeIsNotBinary() {
+        SimpleTree<Integer> tree = new SimpleTree<>(1);
+        assertTrue(tree.add(1, 2));
+        assertTrue(tree.add(1, 3));
+        assertTrue(tree.add(2, 4));
+        assertTrue(tree.add(2, 5));
+        assertTrue(tree.add(3, 6));
+        assertTrue(tree.add(3, 7));
+        assertTrue(tree.add(3, 8));
+        assertFalse(tree.isBinary());
     }
 }
