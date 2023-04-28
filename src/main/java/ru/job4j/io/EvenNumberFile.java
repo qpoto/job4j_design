1ad7c08b4161a0ffc7dac83f29872fa97cabcb42
qpoto <qpoto@yandex.ru>
2023-04-28 13:03:06 +0300

    2.1. FileInputStream [#4898]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -3,7 +3,7 @@ package ru.job4j.io;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-public class EvenNumberFile {
+public class EvenNumberFile  {
     public static void main(String[] args) {
         try (FileInputStream in = new FileInputStream("data/even.txt")) {
             StringBuilder text = new StringBuilder();
