bc7d860727c192b1ee1aea12de003cbcff91a8bf
qpoto <qpoto@yandex.ru>
2023-05-02 08:48:07 +0300

    6. Читаем файл конфигурации [#858]

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -16,19 +16,27 @@ public class Config {
 
     public void load() {
         try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
-            read.lines().filter(line -> line.contains("=")).forEach(line -> {
-                String[] keyValue = line.split("=", 2);
-                String key = keyValue[0];
-                String value = keyValue[1];
-                values.put(key, value);
+            read.lines().filter(line -> line.contains("=")).filter(Config::lineFilter).forEach(line -> {
+                    String[] keyValue = line.split("=", 2);
+                    String key = keyValue[0];
+                    String value = keyValue[1];
+                    values.put(key, value);
             });
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private static boolean lineFilter(String line) {
+        if (!line.startsWith("//") && !line.equals("")) {
+            return true;
+        } else {
+            throw new IllegalArgumentException("Line not good, mr(s) User");
+        }
+    }
+
     public String value(String key) {
-        return key != null ? values.get(key) : null;
+        return values.get(key);
     }
 
     @Override
@@ -40,5 +48,11 @@ public class Config {
             e.printStackTrace();
         }
         return out.toString();
+    }
+
+    public static void main(String[] args) {
+        Config c = new Config("data/app.properties");
+        c.load();
+        System.out.println(c.values.size());
     }
 }
