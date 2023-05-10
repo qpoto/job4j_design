ddcba9d30219412b1a793120523bf6aafae734b1
qpoto <qpoto@yandex.ru>
2023-05-10 12:40:12 +0300

    7. Анализ доступности сервера [#859]

---
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.*;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+
+class AnalysisTest {
+
+    @Test
+    void unavailable(@TempDir Path tempDir) throws IOException {
+        File source = tempDir.resolve("server.log").toFile();
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("400 10:56:01");
+            out.println("300 10:59:01");
+        }
+        File target = tempDir.resolve("target.csv").toFile();
+        Analysis analysis = new Analysis();
+        analysis.unavailable(source.toString(), target.toString());
+
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        String exp = ("10:56:01;10:59:01;");
+        assertThat(exp).isEqualTo(rsl.toString());
+    }
+}
