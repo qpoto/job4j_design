66b4e70d5c29bad0f04452ae3abe8af5f8983132
qpoto <qpoto@yandex.ru>
2023-05-17 14:25:15 +0300

    11. Поиск дубликатов [#315066]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -8,11 +8,23 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    private Map<FileProperty, List<Path>> fPs = new HashMap<>();
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        Map<FileProperty, List<String>> fPs = new HashMap<>();
-        fPs.put(new FileProperty(attrs.size(), file.toString()), new ArrayList<>());
-        System.out.println(fPs);
+        FileProperty fileProperty = new FileProperty(attrs.size(), file.toAbsolutePath().toString());
+        if (!fPs.containsKey(fileProperty)) {
+            fPs.put(fileProperty, new ArrayList<>());
+        }
+        fPs.get(fileProperty).add(file);
+        printPath();
         return super.visitFile(file, attrs);
+    }
+
+    public void printPath() {
+        for (List<Path> path : fPs.values()) {
+            if (path.size() >= 2) {
+                System.out.println(fPs.values());
+            }
+        }
     }
 }
