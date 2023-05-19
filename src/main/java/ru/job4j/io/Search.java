c134221baecd049c5a535996ff8d5eb52b85a692
qpoto <qpoto@yandex.ru>
2023-05-19 10:17:51 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -8,14 +8,34 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
-    public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith("123")).forEach(System.out::println);
+
+    public static boolean validateProgArgs(String[] myArgs) {
+        if (myArgs[0] == null) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        if (!myArgs[1].equals("123")) {
+            throw new IllegalArgumentException("File extension not specified");
+        }
+
+        if (!myArgs[2].contains(".txt")) {
+            throw new IllegalArgumentException("File extension not specified");
+        }
+        return true;
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (validateProgArgs(args)) {
+            Path start = Paths.get(args[0]);
+            search(start, p -> p.toFile()
+                    .getName()
+                    .endsWith(args[1]))
+                    .forEach(System.out::println);
+        }
     }
 }
