4a2f9f5387e0b136cef02c18571b96b46c4b1172
qpoto <qpoto@yandex.ru>
2023-05-28 21:56:21 +0300

    16. Кодировка [#862]

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class ConsoleChat {
+public class ConsoleChat  {
     private static final String OUT = "закончить";
     private static final String STOP = "стоп";
     private static final String CONTINUE = "продолжить";
