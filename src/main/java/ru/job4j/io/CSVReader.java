0b4ed11a7903d5de7025e5b29ec38b299dcdfd9d
qpoto <qpoto@yandex.ru>
2023-05-31 22:11:34 +0300

    17. Scanner [#504791  [#504791 #419347]]

---
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -0,0 +1,36 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+public class CSVReader {
+    public static void handle(ArgsName argsName) throws Exception {
+        Scanner scanner = new Scanner(new FileInputStream(argsName.get("path")))
+                .useDelimiter(argsName.get("delimiter"));
+        String[] strings = argsName.get("filter").split(",");
+        List<String> stringList = new ArrayList<>(Arrays.asList(strings));
+        while (scanner.hasNext()) {
+            stringList.add(scanner.next());
+        }
+        System.out.println(stringList);
+    }
+
+    private static void validateProgArgs(String[] myArgs) {
+        if (myArgs.length != 4) {
+            throw new IllegalArgumentException("В программу должно быть передано 4 аргумента");
+        }
+        if (!(myArgs[1].endsWith(",") || myArgs[1].endsWith(";"))) {
+            throw new IllegalArgumentException("Разделителем может быть запятая или точка с запятой");
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        validateProgArgs(args);
+        ArgsName argsName = ArgsName.of(args);
+        handle(argsName);
+    }
+}
