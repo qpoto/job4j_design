5fd577599130e68b79a36897b9abd8b2a0def7d6
qpoto <qpoto@yandex.ru>
2023-07-19 07:38:53 +0300

    17. Scanner [#504791  [#504791 #419347]]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -5,7 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 
-public class CSVReader {
+public class CSVReader  {
     public static void handle(ArgsName argsName) throws Exception {
         try (Scanner scanner = new Scanner(new FileInputStream(argsName.get("path")))
                 .useDelimiter(argsName.get("delimiter"))) {
@@ -59,8 +59,17 @@ public class CSVReader {
         if (myArgs.length != 4) {
             throw new IllegalArgumentException("В программу должно быть передано 4 аргумента");
         }
+        if (!(Files.exists(Path.of(argsName.get("path"))) && argsName.get("path").endsWith(".csv"))) {
+            throw new IllegalArgumentException("Файл не существует или имеет неверное расширение");
+        }
         if (!(myArgs[1].endsWith(",") || myArgs[1].endsWith(";"))) {
             throw new IllegalArgumentException("Разделителем может быть запятая или точка с запятой");
+        }
+        if (!(argsName.get("out").equals("stdout") || argsName.get("out").endsWith(".csv"))) {
+            throw new IllegalArgumentException("Некорректно введены аргументы вывода результата");
+        }
+        if (argsName.get("filter").endsWith("=")) {
+            throw new IllegalArgumentException("Не указан фильтр");
         }
     }
     public static void main(String[] args) throws Exception {
