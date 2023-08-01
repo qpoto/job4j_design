0f96b679c12272305983ec1dfc120b5b00cc94b6
qpoto <qpoto@yandex.ru>
2023-08-01 07:02:50 +0300

    4. JAXB. Преобразование XML в POJO. [#315063  [#315063]]

--- src/main/java/ru/job4j/serialization/java/Farm.java
+++ src/main/java/ru/job4j/serialization/java/Farm.java
@@ -1,8 +1,18 @@
 package ru.job4j.serialization.java;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class Farm {
+    @XmlElement
     String name;
+    @XmlAttribute
     int cowsQuantity;
+
+    public Farm() {
+    }
 
     public Farm(String name, int cowsQuantity) {
         this.name = name;
--- src/main/java/ru/job4j/serialization/java/Milk.java
+++ src/main/java/ru/job4j/serialization/java/Milk.java
@@ -1,18 +1,34 @@
 package ru.job4j.serialization.java;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 
 import java.util.Arrays;
-
+@XmlRootElement
 public class Milk {
+    @XmlAttribute
     private boolean pasteurized;
+    @XmlAttribute
     private int volume;
+    @XmlAttribute
     private String name;
+    @XmlElement
     private Farm farm;
+    @XmlElement
     private int[] pfc = new int[2];
 
-    public Milk(boolean pasteurized, int volume, String name, Farm farm, int[] pfc) {
+    public Milk() {
+    }
+
+    public Milk(boolean pasteurized, int volume, String name, Farm farm, int... pfc) {
         this.pasteurized = pasteurized;
         this.volume = volume;
         this.name = name;
@@ -32,18 +48,21 @@ public class Milk {
                 + '}';
     }
 
-    public static void main(String[] args) {
-        Farm kozino = new Farm("Kozino", 100);
-        int[] pfc = {3, 3, 5};
-        Milk parmalat = new Milk(true, 1, "Parmalat", kozino, pfc);
-
-        Gson gson = new GsonBuilder().create();
-        String milk = gson.toJson(parmalat);
-        System.out.println(milk);
-
-        Milk parmalatJson = gson.fromJson(milk, Milk.class);
-        System.out.println(parmalatJson);
-
-
+    public static void main(String[] args) throws JAXBException, IOException {
+        Milk milk = new Milk(true, 1, "Milk", new Farm("Kozino", 100), 3, 3, 3);
+        JAXBContext context = JAXBContext.newInstance(Milk.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml = "";
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(milk, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        }
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            Milk result = (Milk) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
     }
 }
