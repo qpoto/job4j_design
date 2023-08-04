741bb4373d429362f41b21c9b3800faadb459cdc
qpoto <qpoto@yandex.ru>
2023-08-04 08:10:58 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064  [#315064 #433758]]

---
+++ db/script.sql
@@ -0,0 +1,6 @@
+insert into staff(name, lastname, year) values('Slava', 'Mikhailov', '1988');
+select * from staff;
+update staff set name = 'Stanislav';
+select * from staff;
+delete from staff;
+select * from staff;
