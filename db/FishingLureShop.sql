72571d448d700edd8e41ba215f2b9ff377897ce1
qpoto <qpoto@yandex.ru>
2023-08-10 08:17:50 +0300

    1. Связи между таблицами [#373615 #434546]

---
+++ db/FishingLureShop.sql
@@ -0,0 +1,29 @@
+create table SoftLures(
+    id serial primary key,
+    sort varchar(255)
+);
+
+insert into SoftLures(sort) values ('Силикон');
+insert into SoftLures(sort) values ('Стример');
+
+select * from SoftLures;
+
+create table HardLures(
+    id serial primary key,
+    sort varchar(255)
+);
+
+insert into HardLures(sort) values ('Воблер');
+insert into HardLures(sort) values ('Блесна');
+
+select * from HardLures;
+
+create table Brand(
+    id serial primary key,
+    brand varchar(255)
+);
+
+insert into Brand(brand) values ('HandMade');
+insert into Brand(brand) values ('Evergreen');
+
+select * from Brand;
