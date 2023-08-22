e0f864bac7085f8b7a237418e1ece0db1aa98589
qpoto <qpoto@yandex.ru>
2023-08-22 07:47:06 +0300

    2. Схема прав пользователи и роли. [#1093 #436750]

--- db/create.sql
+++ db/create.sql
@@ -3,11 +3,12 @@ create table if not exists comments(id serial primary key, name text);
 create table if not exists attachs(id serial primary key, name text);
 create table if not exists items(id serial primary key, name text,
 				  comments_id int references comments(id),
-				  attachs_id int references attachs(id));
-create table if not exists users (id serial primary key, name text,
-				  items_id int references items(id));
-create table if not exists roles(id serial primary key, name text,
+				  attachs_id int references attachs(id),
 				  users_id int references users(id));
+create table if not exists users (id serial primary key, name text,
+                  roles_id int references roles(id),
+				  items_id int references items(id));
+create table if not exists roles(id serial primary key, name text);
 create table if not exists roles_rules(id serial primary key,
 						roles_id int references roles(id),
 						rules_id int references rules(id));
