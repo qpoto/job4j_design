e1bdd873ce4664b85e68e74753e4025245c94322
qpoto <qpoto@yandex.ru>
2023-08-23 07:56:59 +0300

    2. Схема прав пользователи и роли. [#1093 #436750]

--- db/create.sql
+++ db/create.sql
@@ -1,21 +1,22 @@
-create table if not exists rules(id serial primary key, name text);
-create table if not exists comments(id serial primary key, name text);
-create table if not exists attachs(id serial primary key, name text);
-create table if not exists items(id serial primary key, name text,
-				  comments_id int references comments(id),
-				  attachs_id int references attachs(id),
-				  users_id int references users(id));
-create table if not exists users (id serial primary key, name text,
-                  roles_id int references roles(id),
-				  items_id int references items(id));
 create table if not exists roles(id serial primary key, name text);
+create table if not exists rules(id serial primary key, name text);
 create table if not exists roles_rules(id serial primary key,
 						roles_id int references roles(id),
 						rules_id int references rules(id));
-create table if not exists categories(id serial primary key, name text,
-						items_id int references items(id));
-create table if not exists states(id serial primary key, name text,
-					items_id int references items(id));
+create table if not exists comments(id serial primary key, name text);
+create table if not exists attachs(id serial primary key, name text);
+create table if not exists categories(id serial primary key, name text);
+create table if not exists states(id serial primary key, name text);
+create table if not exists users (id serial primary key, name text,
+                  roles_id int references roles(id));
+create table if not exists items(id serial primary key, name text,
+				  comments_id int references comments(id),
+				  attachs_id int references attachs(id),
+				  categories_id int references categories(id),
+				  states_id int references states(id),
+				  users_id int references users(id));
+
+
 
 
 
--- db/insert.sql
+++ db/insert.sql
@@ -1,12 +1,15 @@
+insert into roles(name) VALUES ('роль_1');
 insert into rules(name) VALUES ('права_1');
+insert into roles_rules(roles_id, rules_id) VALUES (1, 1);
 insert into comments(name) VALUES ('комментарий_1');
 insert into attachs(name) VALUES ('вложение_1');
-insert into items(name, comments_id, attachs_id) VALUES ('заявка_1', 1, 1);
-insert into users(name, items_id) VALUES ('пользователь_1', 1);
-insert into roles(name, users_id) VALUES ('роль_1', 1);
-insert into roles_rules(roles_id, rules_id) VALUES (1, 1);
-insert into categories(name, items_id) VALUES ('категория_1', 1);
-insert into states(name, items_id) VALUES ('категория_1', 1);
+insert into categories(name) VALUES ('категория_1');
+insert into states(name) VALUES ('состояние_1');
+insert into users(name, roles_id) VALUES ('пользователь_1', 1);
+insert into items(name, comments_id, attachs_id, categories_id, states_id, users_id) VALUES ('заявка_1', 1, 1, 1, 1, 1);
+
+
+
 
 
 
