6530560bb70825aaaa4de5fe24e3bc35e36a69f7
qpoto <qpoto@yandex.ru>
2023-08-24 06:44:56 +0300

    2. Схема прав пользователи и роли. [#1093 #436750]

--- db/insert.sql
+++ db/insert.sql
@@ -1,12 +1,12 @@
 insert into roles(name) VALUES ('роль_1');
 insert into rules(name) VALUES ('права_1');
 insert into roles_rules(roles_id, rules_id) VALUES (1, 1);
-insert into comments(name) VALUES ('комментарий_1');
-insert into attachs(name) VALUES ('вложение_1');
 insert into categories(name) VALUES ('категория_1');
 insert into states(name) VALUES ('состояние_1');
 insert into users(name, roles_id) VALUES ('пользователь_1', 1);
-insert into items(name, comments_id, attachs_id, categories_id, states_id, users_id) VALUES ('заявка_1', 1, 1, 1, 1, 1);
+insert into items(name, categories_id, states_id, users_id) VALUES ('заявка_1', 1, 1, 1);
+insert into comments(name, items_id) VALUES ('комментарий_1', 1);
+insert into attachs(name, items_id) VALUES ('вложение_1', 1);
 
 
 
