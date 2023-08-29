0b006658662aaf6ba41552c1f267eb260e940055
qpoto <qpoto@yandex.ru>
2023-08-29 07:31:47 +0300

    3. Фильтры [#1732 #440337]

--- db/Скрипт к уроку 3. Фильтры [#1732].sql
+++ db/Скрипт к уроку 3. Фильтры [#1732].sql
@@ -1,13 +1,20 @@
 select * from product as p join type as t on p.type_id = t.id where t.name like 'Сыр';
+
 select * from product where name like '%мороженое%';
+
 select * from product where expired_date > current_date;
-select max(price) from product;
+
+select * from product  where price=(select MAX(price) from product);
+
 select t.name, count(t.name) from product as p join type as t on p.type_id = t.id group by t.name; 
+
 select * from product as p join type as t on p.type_id = t.id where t.name like 'Сыр' or t.name like 'Молоко';
-select p.name, t.name from product as p join type as t on p.type_id = t.id;
-select p.name, t.name from product as p join type as t on p.type_id = t.id;
 
+select tname, count(tname) from (select t.name tname from type t inner join product p on t.id=p.type_id) tt
+group by tname
+having count(tname)<10;
 
+select p.name, t.name from product as p join type as t on p.type_id = t.id;
 
 
 
